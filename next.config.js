2bca08581795f37be2db593d4fe08320b8fb3dbb
shibincreji <scr2404@gmail.com>
2024-12-05 20:07:35 +0530

    Fix header validation for Netlify

--- next.config.js
+++ next.config.js
@@ -1,4 +1,23 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    serverActions: {
+      validateHeaders: false, // Disable strict header validation for Server Actions
+    },
+  },
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "X-Forwarded-Host",
+            value: "hyzync-v5.netlify.app", // Use your Netlify domain here
+          },
+        ],
+      },
+    ];
+  },
+};
 
 module.exports = nextConfig;
